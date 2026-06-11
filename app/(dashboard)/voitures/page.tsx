17424c4d283ba4be6fd31c15f766d74223b7d0c8
Lyne21 <gbaguidilyne83@gmail.com>
2026-06-11 18:21:46 +0200

    début mise en place visuel du catalogue

--- app/(dashboard)/voitures/page.tsx
+++ app/(dashboard)/voitures/page.tsx
@@ -1,9 +1,95 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 const page = () => {
+
+ const catalogueVoiture = [
+   {
+     id : "1",
+     marque: "Toyota",
+     type: "SUV",
+     tarif: 40000,
+     disponible: true,
+     image: "",
+   },
+
+   {
+     id: "2",
+     marque: "Tesla",
+     type: "SUV",
+     tarif: 40000,
+     disponible: true,
+     image: "",
+   },
+
+   {
+     id: "3",
+     marque: "RAV4",
+     type: "SUV",
+     tarif: 40000,
+     disponible: true,
+     image: "",
+   },
+
+   {
+     id: "4",
+     marque: "Lomborghini",
+     type: "SUV",
+     tarif: 40000,
+     disponible: false,
+     image: "",
+   },
+ ]
+  
   return (
     <div>
-      page Voiture
+      {/* Filter */}
+      <div>
+        
+      </div>
+
+      {/* Ctalogue */}
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        {catalogueVoiture.map ((voiture) =>(
+        
+        <Card key={voiture.id} className='w-full max-w-sm overflow-hidden flex flex-col justify-between'>
+
+          <CardHeader className="p-0"> 
+            <div className="relative w-full  bg-gray-100">
+              {voiture.image}
+            </div>
+          </CardHeader>
+
+          <CardContent className="p-4 space-y-2">
+            <div className="flex justify-between items-center">
+              <CardTitle className="text-xl font-bold">{voiture.marque}</CardTitle>
+              <Badge>{voiture.disponible}</Badge>
+            </div>
+            <CardDescription>{voiture.type}</CardDescription>
+          </CardContent>
+
+          <CardFooter className="p-4 border-t flex justify-between items-center">
+            <div>
+              <p className="text-xs text-gray-500">Tarif</p>
+              <p className="text-lg font-bold text-blue-600">{voiture.tarif} FCFA <span className="text-xs font-normal text-gray-500">/ jour</span></p>
+            </div>
+            <Button>Voir</Button>
+          </CardFooter>
+
+        </Card>
+        ))}
+      </div>
     </div>
   )
 }
